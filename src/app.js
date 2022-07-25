75060320333f97a79855cdad28f5c0a3b2534e69
gabriel sanchez <gabrielpelota.8@gmail.com>
2022-07-25 18:26:04 -0500

    cambios

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ app.use(express.json());
 
 app.use('/todolist/v1', taskRouter)
 
-//* localhost:8000/todo-list/v1/tasks
+//* localhost:8000/todolist/v1/tasks
 
 app.listen(8000, () => {
     console.log("Server started at port 8000");
